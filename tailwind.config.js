27bd026a484890cc99b7952292bf111773b76fcb
codewrangler-yasmin <mahdiaayasmin@gmail.com>
2024-10-04 05:59:51 +0600

    Created the workspace

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./public/js/script.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+      },
+      fontFamily: {
+        primary: [],
+        secondary: [],
+      },
+    },
+  },
+  plugins: [],
+};
